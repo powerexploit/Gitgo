3e6307fb369dd69afd2afa9a4b9789b13fe79c28
ankitdobhal <dobhal.ankit@protonmail.com>
2020-09-12 06:49:37 -0400

    Updated Gitgo py file

--- Gitgo.py
+++ Gitgo.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python3
-import requests as res
+import requests
 import bs4 
 import argparse
-from colorama import Fore, Back, Style
+from colorama import Fore
 
 def gitpinnedrepo(username):
-	res = requests.get("https://github.com/%s" %(username)).text
-	soup = bs4.BeautifulSoup(res.txt,"html.parser")
+	res = requests.get("https://github.com/%s" %(username))
+	soup = bs4.BeautifulSoup(res.text,"html.parser")
 	pinned = soup.find_all("span", class_='repo')
 	for pinned_repo in range(len(pinned)):
-		return pinned[pinned_repo].text
-
+		print(Fore.WHITE + pinned[pinned_repo].text)
 
 
 if __name__ == '__main__':
-	main()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--gitusername',help='Github username')
+	args = parser.parse_args()
+	print(Fore.RED + '''
+
+
+  ▄████  ██▓▄▄▄█████▓  ▄████  ▒█████  
+ ██▒ ▀█▒▓██▒▓  ██▒ ▓▒ ██▒ ▀█▒▒██▒  ██▒
+▒██░▄▄▄░▒██▒▒ ▓██░ ▒░▒██░▄▄▄░▒██░  ██▒
+░▓█  ██▓░██░░ ▓██▓ ░ ░▓█  ██▓▒██   ██░
+░▒▓███▀▒░██░  ▒██▒ ░ ░▒▓███▀▒░ ████▓▒░
+ ░▒   ▒ ░▓    ▒ ░░    ░▒   ▒ ░ ▒░▒░▒░ 
+  ░   ░  ▒ ░    ░      ░   ░   ░ ▒ ▒░ 
+░ ░   ░  ▒ ░  ░      ░ ░   ░ ░ ░ ░ ▒  
+      ░  ░                 ░     ░ ░   > v1.0
+
+''')
+
+	if args.gitusername:
+		gitpinnedrepo(args.gitusername)
+		exit()
